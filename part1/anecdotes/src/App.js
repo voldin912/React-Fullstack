b09150423e45c413c97ddc03864107adc05f56f1
kishidakohei <kohei99624@gmail.com>
2022-10-25 17:33:21 +0300

    add anecdote project and complete 1.12* / step 1

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const nextAnecdotes = () => {
+    const randomNum = Math.floor(Math.random() * 7);
+    setSelected(randomNum)
+  }
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <button onClick={nextAnecdotes}>next anecdote</button>
+    </div>
+  );
+}
+
+export default App;
