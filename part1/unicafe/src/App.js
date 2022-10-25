bcafb8d1327d0994e5aecdd1183872e60d3882e4
kishidakohei <kohei99624@gmail.com>
2022-10-25 14:24:44 +0300

    Add unicafe to part1, complete step 1 - 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+const Button = (props) => {
+  const {handleClick, text} = props
+  return (
+    <button onClick={handleClick}> {text} </button>
+  )
+}
+const Statistic = (props) => {
+  const {goodData, neutralData,badData} = props
+  return(
+    <div>
+      <h2>statistics</h2>
+      <p>good {goodData}</p>
+      <p>neutral {neutralData}</p>
+      <p>bad {badData}</p>
+    </div>
+  )
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const clickGood = () => {
+    setGood(good + 1)
+  } 
+  const clickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const clickBad = () => {
+    setBad(bad + 1)
+  }
+  return (
+    <div class='App'>
+      <h1>give feedback</h1>
+      <Button handleClick={clickGood} text="good"/>
+      <Button handleClick={clickNeutral} text="neutral"/>
+      <Button handleClick={clickBad} text="bad"/>
+      <Statistic goodData={good} neutralData={neutral} badData={bad}/>
+    </div>
+  )
+}
+
+export default App;
