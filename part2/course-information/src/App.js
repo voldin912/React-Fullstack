a54c858a31154a77aafd38acce6c9c7101c7f7cd
kishidakohei <kohei99624@gmail.com>
2022-10-28 17:09:39 +0300

    complete course information step 7/ 2.2

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -1,6 +1,6 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
 
 const Part = ({ part }) => 
   <p>
@@ -12,12 +12,14 @@ const Content = ({ parts }) =>
     {parts.map(part => <Part key={part.id} part={part}/>)}
   </>
 const Course = ({course}) => {
-    return (
-      <>
-        <Header course = {course.name}/>
-        <Content parts = {course.parts}/>
-      </>
-    )
+  const exerciseSum = course.parts.map(part => part.exercises).reduce((a,b) => a + b,0);
+  return (
+    <>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total sum = {exerciseSum}/>
+    </>
+  )
 
 }
 const App = () => {
