92c223ed58ab57117d28cf864d44aef58b79bf7a
kishidakohei <kohei99624@gmail.com>
2022-11-01 18:20:05 +0200

    complete step 2/ 2.13*

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
+
+//  Country component that displays specific information of a country
 const Country = ({country})  => {
   const languagesList = Object.values(country.languages)
   return (
@@ -16,19 +18,33 @@ const Country = ({country})  => {
     )
 }
 
-const FoundCountries = ({countries}) => {
+// CountryListDisplay component that displays list of found countries from queries
+const CountryListDisplay = ({country,clickListener}) => {
+  return (
+    <div>
+      {country.name.common} <button style={{display:"inline-block"}} onClick={() => clickListener(country)}>Show</button>
+    </div>
+  )
+
+}
+// component that renders CountryListDisplay 
+const FoundCountries = ({countries,handleShowButtonClick}) => {
   return (
     <ul>
-      {countries.map(country => <li key={country.name.official}>{country.name.common}</li>)}
+      {countries.map(country => <CountryListDisplay key={country.name.official} country={country} clickListener={handleShowButtonClick}/>)}
     </ul>
   )
 }
 
+// Main app component
 const App = () => {
   const [query, setQuery] = useState("")
   const [countries, setCountries] = useState([])
   const handleQueryChange = (event) => {
     setQuery(event.target.value)
+  }
+  const handleShowButtonClick = (selectedCountry) => {
+    setQuery(selectedCountry.name.common)
   }
   useEffect (() => {
     axios
@@ -40,10 +56,10 @@ const App = () => {
   },[])
 
   const runQuery = (query) => {
-    if(countries.length != 0 && query.length != 0) {
-      const result = countries.filter(country => country.name.common.toLowerCase().includes(query))
+    if(countries.length !== 0 && query.length !== 0) {
+      const result = countries.filter(country => country.name.common.toLowerCase().includes(query.toLowerCase()))
       return (result.length > 10) ?  <p>Too many matches, specify another filter</p>
-      : (result.length > 1 && result.length < 10) ? <FoundCountries countries = {result}/>
+      : (result.length > 1 && result.length < 10) ? <FoundCountries countries = {result} handleShowButtonClick={handleShowButtonClick}/>
       : (result.length === 1) ? <Country country={result[0]}/>
       : <p>Query not found</p>
     }
