31550e9362cca66005979e7274e68fb1af0a0dd2
kishidakohei <kohei99624@gmail.com>
2022-11-12 21:21:18 +0200

    complete phonebook backend step8/ 3.8*

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -23,8 +23,10 @@ let persons = [
         "number": "39-23-6423122"
       }
 ]
+
 app.use(express.json())
-app.use(morgan("tiny"))
+morgan.token('tiny', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :tiny"))
 
 app.get("/api/persons",(req,res) => {
     res.json(persons)
