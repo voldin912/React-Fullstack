0d08bed75b8bc5e36a8c3b331b4c0b7a6fe67223
kishidakohei <kohei99624@gmail.com>
2022-11-12 20:55:56 +0200

    complete phonebook backend step7 / 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 let persons = [
     { 
         "id": 1,
@@ -23,9 +24,12 @@ let persons = [
       }
 ]
 app.use(express.json())
+app.use(morgan("tiny"))
+
 app.get("/api/persons",(req,res) => {
     res.json(persons)
 })
+
 app.get("/info", (req,res) => {
     const current_time = new Date().toString()
     const numberPerson = persons.length
