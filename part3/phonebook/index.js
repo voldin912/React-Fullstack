28932f41e8915b5a68889cfae9c76569bda28a1b
kishidakohei <kohei99624@gmail.com>
2022-11-14 21:21:41 +0200

    use middleware for build folder

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,7 @@ app.use(express.json())
 morgan.token('tiny', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :tiny"))
 app.use(cors())
+app.use(express.static("build"))
 
 app.get("/api/persons",(req,res) => {
     res.json(persons)
