06f10fa130c5586a4bf288159ed181c1aaa25cd1
kishidakohei <kohei99624@gmail.com>
2023-03-01 18:04:11 +0200

    remove the warining in export default

--- part2/phonebook/src/services/personService.js
+++ part2/phonebook/src/services/personService.js
@@ -1,25 +1,26 @@
-import axios from "axios"
-const server_url = "/api/persons"
+import axios from "axios";
+const server_url = "/api/persons";
 const getPersons = () => {
-   const request = axios.get(server_url)
-   return request.then(response => response.data)
-}
+  const request = axios.get(server_url);
+  return request.then((response) => response.data);
+};
 
 const addPerson = (newPerson) => {
-    const request = axios.post(server_url,newPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.post(server_url, newPerson);
+  return request.then((response) => response.data);
+};
 
 const deletePerson = (deleteId) => {
-    return axios.delete(`${server_url}/${deleteId}`)
-}
-const updatePerson = (updateId,newPerson) => {
-    const request = axios.put(`${server_url}/${updateId}`,newPerson)
-    return request.then(response => response.data)
-}
-export {
-    getPersons,
-    addPerson,
-    deletePerson,
-    updatePerson
-}
+  return axios.delete(`${server_url}/${deleteId}`);
+};
+const updatePerson = (updateId, newPerson) => {
+  const request = axios.put(`${server_url}/${updateId}`, newPerson);
+  return request.then((response) => response.data);
+};
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getPersons,
+  addPerson,
+  deletePerson,
+  updatePerson,
+};
