5b386687aed2d9d128cc0403b79155197ea22045
kishidakohei <kohei99624@gmail.com>
2023-03-05 18:26:38 +0200

    add Lint configuration. Complete 3.22

---
+++ part3/phonebook/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console':0
+  },
+};
