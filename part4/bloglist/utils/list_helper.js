58b9cdba6d5661668771c8a564b86115b02e11d4
kishidakohei <kohei99624@gmail.com>
2023-03-06 17:48:19 +0200

    Add favoriteBlog function and unit testing. Complete 4.5*

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,7 +9,23 @@ const totalLikes = (blogs) => {
   return result;
 };
 
+const favoriteBlog = (blogs) => {
+  const result = blogs
+    .reduce((accumulator,current) => {
+      if(!accumulator.length || accumulator[0].likes < current.likes) {
+        delete current.__v;
+        delete current._id;
+        delete current.url;
+        accumulator[0] = current;
+      }
+      return accumulator;
+    }, []);
+  console.log(result);
+  return result[0] || 0;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
