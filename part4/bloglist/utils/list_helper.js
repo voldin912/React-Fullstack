557669d6e8dbdaecef7946967204e9ba08e14912
kishidakohei <kohei99624@gmail.com>
2023-03-06 16:43:38 +0200

    complete 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const result = blogs
+    .map(blog => blog.likes)
+    .reduce((acc,cur) => acc + cur,0);
+  return result;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
